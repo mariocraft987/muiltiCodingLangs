6fb016e3f4dbd651ca1399d83d0e270ee77c6df1
mariocraft <154646419+mariocraft987@users.noreply.github.com>
2024-04-10 16:06:09 -0700

    Update function.go

--- function.go
+++ function.go
@@ -2,6 +2,9 @@ package main
 import ("fmt")
 
 func main() {
-  fmt.Println("Hello World!")
+  fmt.Println("Hello, World!")
   fmt.Println("Wow this works!")
+  fmt.Println("e")
 }
+
+// This is a function
