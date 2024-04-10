001bf32577ce04f00272dcea67097669b009d0a4
mariocraft <154646419+mariocraft987@users.noreply.github.com>
2024-04-10 16:08:20 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,3 +3,10 @@
 alert('Hello, World')
 
 console.log("This is created using Javascript!")
+
+// event = keyup or keydown
+document.addEventListener('keyup', event => {
+  if (event.code === 'Space') {
+    console.log('Space pressed')
+  }
+})
